b438037d3b9353a2db026f8b12e39c2f7faff1d2
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2021-01-09 21:05:08 +0100

    [UPDATE] WIP profil

--- src/components/profil/profil.js
+++ src/components/profil/profil.js
@@ -12,10 +12,18 @@ export const Profil = (props) => {
             <div className="container mt-2 black-background rounded p-3">
                 <h3 className="mt-3 mb-2 white-text text-center">Vous n'avez pas de profil ?</h3>
                 <div className="d-flex align-items-center justify-content-center">
-                    <button className="btn btn-lg rainbow-box white-text mb-3 lead">
+                    <button className="btn btn-lg rainbow-box white-text mb-3 lead" data-toggle="collapse" data-target="#collapseContainer" > 
                         <i className="fa fa-plus rainbow"> </i>  Creer un profile
                     </button>
                 </div>
+
+                <div class="collapse" id="collapseContainer">
+                        <div class="card card-body">
+                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+                        </div>
+                    </div>
+
+                    
             </div>
         </div>
 
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,48 +1,131 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useRef} from 'react'
 import Menu from "../Menu";
 import * as THREE from "three";
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
+import { Interaction } from 'three.interaction';
 
 
 export const Search = (props) => {
+    const isMounted = useRef(false);
+
+    // Rendre cliquable les elements 
+    const canvasContainer = useRef(null);
+
+    const [scene, setScene] = React.useState(null);
+    const [camera, setCamera] = React.useState(null);
+    const [renderer, setRenderer]  = React.useState(new THREE.WebGLRenderer());
 
     useEffect( () => {
-        // === THREE.JS CODE START ===
-        const ambiantLight = new THREE.AmbientLight( 0xff0000, 5 );
-        const light = new THREE.PointLight( 0xff0000, 20, 100 );
-        light.position.set( 1, 1, 1);
 
+        if (isMounted.current) {
+                console.log('fetching');
+          } else {
+                isMounted.current = true;
+                setScene(new THREE.Scene());
+                setCamera(new THREE.PerspectiveCamera(
+                    75,
+                    window.innerWidth/window.innerHeight,
+                    0.1,
+                    1000
+                ));
 
-        let scene = new THREE.Scene();
-        let camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-        let renderer = new THREE.WebGLRenderer();
-        renderer.setSize( window.innerWidth, window.innerHeight );
-        document.body.appendChild( renderer.domElement );
-        let geometry = new THREE.BoxGeometry( 1, 1, 1 );
-        let material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-        let cube = new THREE.Mesh( geometry, material );
+                renderer.setSize(window.innerWidth, window.innerHeight);
+                canvasContainer.current.appendChild( renderer.domElement );
 
-        //scene.add( cube );
-        scene.add( light );
-        scene.add( ambiantLight );
+        }
 
-        camera.position.z = 5;
-        const animate = function () {
-          requestAnimationFrame( animate );
-          cube.rotation.x += 0.01;
-          cube.rotation.y += 0.01;
-          console.log(light);
-          renderer.render( scene, camera );
-        };
         animate();
-        console.log("ANIMATION")
-        }, []);
+
+    }, []);
+
+    const animate = function () {
+        console.log("animate");
+        console.log(scene);
+        requestAnimationFrame( animate );
+    };
+
+    if ( scene != null ) {
+        if ( camera != null ) {
+            const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+            const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+            const cube = new THREE.Mesh( geometry, material );
+
+            camera.position.z = 5;
+            scene.add( cube );
+
+
+            renderer.render( scene, camera );
+        }
+    }
+
+    /*
+    const animate = function () {
+        requestAnimationFrame( animate );
+
+    
+        //   cube.rotation.x += 0.01;
+        //   cube.rotation.y += 0.01;
+
+
+        //   cube2.rotation.x += 50;
+
+        //sphere.position.x += 0.05;
+        //sphere.position.x -= 0.08;
+
+        //console.log(scene.children)
+
+        //controls.update();
+        console.log("ICI")
+        console.log(scene, camera)
+        renderer.render( scene, camera );
+
+    };*/
+
+
+
+
+    // useEffect( () => {
+    //     var scene = new THREE.Scene();
+    //     var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+    //     var renderer = new THREE.WebGLRenderer();
+    //     renderer.setSize( window.innerWidth, window.innerHeight );
+    //     // document.body.appendChild( renderer.domElement );
+    //     // use ref as a mount point of the Three.js scene instead of the document.body
+    //     this.mount.appendChild( renderer.domElement );
+    //     var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    //     var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    //     var cube = new THREE.Mesh( geometry, material );
+    
+
+    //     const animate = function () {
+    //       requestAnimationFrame( animate );
+
+    
+    //     //   cube.rotation.x += 0.01;
+    //     //   cube.rotation.y += 0.01;
+
+
+    //     //   cube2.rotation.x += 50;
+
+    //       //sphere.position.x += 0.05;
+    //       //sphere.position.x -= 0.08;
+
+    //       //console.log(scene.children)
+
+    //     //controls.update();
+    //     renderer.render( scene, camera );
+
+    // };
+    //     animate();
+    //     }, []);
     return (
 
         <div className="">
             <header className="">
                 <Menu></Menu>
             </header>
-     
+            <div  ref={canvasContainer}>
+            </div>
         </div>
 
     )
